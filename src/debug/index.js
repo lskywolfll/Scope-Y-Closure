5f3d086dad5c1ca9ce0a9cb2baf9cac193622daf
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-02-12 23:45:22 -0300

    se termina eÃl debug

---
+++ src/debug/index.js
@@ -0,0 +1,34 @@
+var saludo = 'Hello';
+
+function hello(){
+    let saludoCompleto = 'Hello World';
+    const saludoCompletoGritandoBajo = 'Hello World!';
+    if(true){
+        let saludoCompletoGritandoMedio = 'Hello World!!';
+        debugger
+    }
+}
+
+hello();
+
+const moneyBox = () => {
+    var saveCoins = 0;
+    var saveCoinsTest = 0;
+    const countCoins = (coins) => {
+        saveCoins += coins;
+        console.log(`MoneyBox: $${saveCoins}`);
+    };
+
+    const countTest = (coins, other) => {
+        saveCoinsTest += coins + other;
+        console.log(`saveCoinsTest: ${saveCoinsTest}`);
+    };
+    // el return ejecuta la funcion anonima con los parametros que piden por el cual nosotros ingresamos por el moneybox
+    return countCoins
+};
+
+let myMoneyBox = moneyBox();
+
+myMoneyBox(4);
+myMoneyBox(6);
+myMoneyBox(10);
